859fe28c5aea27ba391db904a79c48e64db4d1df
stephengrider <ste.grider@gmail.com>
2017-03-17 11:02:39 -0700

    added ball animation

--- swipe/main.js
+++ swipe/main.js
@@ -20,9 +20,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
 
--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 }
+    }).start();
+  }
+
   render() {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     );
   }
 }
