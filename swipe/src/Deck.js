7ae06473ad710ff5ca6f37c6b329ff836cc515ce
stephengrider <ste.grider@gmail.com>
2017-03-20 11:42:47 -0700

    animated top card

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -22,19 +22,27 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
-      <Animated.View
-        style={this.state.position.getLayout()}
-        {...this.state.panResponder.panHandlers}
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
     );
   }
 }
