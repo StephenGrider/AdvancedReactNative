c3edf8d90c7dcfa1c6337eeb72d56bcb5676bd23
stephengrider <ste.grider@gmail.com>
2017-03-21 10:27:50 -0700

    card rotates

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -21,12 +21,20 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
+  getCardStyle() {
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate: '-45deg' }]
+    };
+  }
+
   renderCards() {
     return this.props.data.map((item, index) => {
       if (index === 0) {
         return (
           <Animated.View
-            style={this.state.position.getLayout()}
+            key={item.id}
+            style={this.getCardStyle()}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
