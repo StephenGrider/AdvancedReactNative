b1d5761d96edcd1d1ddb71246671f26a97d56647
stephengrider <ste.grider@gmail.com>
2017-03-21 11:53:59 -0700

    force swipe

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -22,9 +22,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          this.forceSwipeRight();
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left!')
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -34,9 +34,10 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
-  forceSwipeRight() {
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(this.state.position, {
-      toValue: { x: SCREEN_WIDTH, y: 0 },
+      toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION
     }).start();
   }
