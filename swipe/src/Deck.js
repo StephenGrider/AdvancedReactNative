2d54d6ca79353c4ccd625758ee68f77396922b8f
stephengrider <ste.grider@gmail.com>
2017-03-22 11:43:46 -0700

    cascade card list

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -96,9 +96,12 @@ class Deck extends Component {
       }
 
       return (
-        <View key={item.id} style={styles.cardStyle}>
+        <Animated.View
+          key={item.id}
+          style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}
+        >
           {this.props.renderCard(item)}
-        </View>
+        </Animated.View>
       );
     }).reverse();
   }
