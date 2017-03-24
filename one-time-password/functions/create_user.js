f2d47d6965683872e263d0dfa3673bf2a71461fc
stephengrider <ste.grider@gmail.com>
2017-03-24 11:45:37 -0700

    create new user

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -10,6 +10,9 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   // Create a new user account using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 
   // Respond to the user request, saying the account was made
 }
