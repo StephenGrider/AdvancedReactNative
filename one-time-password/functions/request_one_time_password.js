f154a2dec999eea5d749b2a0dbb669658bbc3aa2
stephengrider <ste.grider@gmail.com>
2017-03-24 12:54:27 -0700

    sent text message

--- one-time-password/functions/request_one_time_password.js
+++ one-time-password/functions/request_one_time_password.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const twilio = require('./twilio');
 
 module.exports = function(req, res) {
   if (!req.body.phone) {
@@ -9,6 +10,15 @@ module.exports = function(req, res) {
 
   admin.auth().getUser(phone)
     .then(userRecord => {
-      
+      const code = Math.floor((Math.random() * 8999 + 1000));
+
+      twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: phone,
+        from: '+18053167032'
+      })
     })
+    .catch((err) => {
+      res.status(422).send({ error: err });
+    });
 }
