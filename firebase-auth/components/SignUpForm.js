289ee5a20e46e564ce1005ada9c35b7e494d564a
stephengrider <ste.grider@gmail.com>
2017-03-24 14:55:21 -0700

    added instnace props

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -3,14 +3,23 @@ import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+
+  handleSubmit = () => {
+    
+  }
+
   render() {
     return (
       <View>
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
-          <FormInput />
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
         </View>
-        <Button title="Submit" />
+        <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }
