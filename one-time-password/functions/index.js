c1c0f5cea5856a8c278e4041191ccdc9e238d186
stephengrider <ste.grider@gmail.com>
2017-03-24 10:43:54 -0700

    added goodbye function

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -6,3 +6,7 @@ var functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 })
+
+exports.goodbye = functions.https.onRequest((request, response) => {
+  response.send("Goodbye!");
+});
