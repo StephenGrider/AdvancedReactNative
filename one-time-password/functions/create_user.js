55095b8137e0e7de4f23790830400ff022345f0f
stephengrider <ste.grider@gmail.com>
2017-03-24 11:38:40 -0700

    sanitize user inputs

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,15 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // Format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create a new user account using that phone number
+
+  // Respond to the user request, saying the account was made
 }
