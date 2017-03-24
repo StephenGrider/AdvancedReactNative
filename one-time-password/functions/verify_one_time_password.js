c60b86053970474f5731f0204054773b77a255c3
stephengrider <ste.grider@gmail.com>
2017-03-24 13:30:19 -0700

    invalidate used code

--- one-time-password/functions/verify_one_time_password.js
+++ one-time-password/functions/verify_one_time_password.js
@@ -1,3 +1,5 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
   if (!req.body.phone || !req.body.code) {
     return res.status(422).send({ error: 'Phone and code must be provided'});
@@ -6,5 +8,18 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
   const code = parseInt(code);
 
-  
+  admin.auth().getUser(phone)
+    .then(() => {
+      const ref = admin.database().ref('users/' + phone);
+      ref.on('value', snapshot => {
+        const user = snapshot.val();
+
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: 'Code not valid' });
+        }
+
+        ref.update({ codeValid: false });
+      });
+    })
+    .catch((err) => res.status(422).send({ error: err }))
 }
