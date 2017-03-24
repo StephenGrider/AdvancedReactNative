ab832a88d5e4b9cfd5eee02172bce7942c1c25f4
stephengrider <ste.grider@gmail.com>
2017-03-24 15:03:42 -0700

    call cloud funcs

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-c725a.cloudfunctions.net';
 
 class SignUpForm extends Component {
   state = { phone: '' };
 
   handleSubmit = () => {
-    
+    axios.post(`${ROOT_URL}/createUser`, {
+      phone: this.state.phone
+    })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone })  
+      })
   }
 
   render() {
