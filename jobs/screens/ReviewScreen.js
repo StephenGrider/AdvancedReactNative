1eecd1da0d001e8578cf79b5b892c45ea0d0f029
stephengrider <ste.grider@gmail.com>
2017-03-29 11:52:59 -0700

    added class prop

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+
+  }
+    
   render() {
     return (
       <View>
