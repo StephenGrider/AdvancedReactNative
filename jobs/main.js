3f88c4f31aaecd7b49a4e34ecfae651d3570fe7d
stephengrider <ste.grider@gmail.com>
2017-03-29 11:29:05 -0700

    nested navigator

--- jobs/main.js
+++ jobs/main.js
@@ -5,12 +5,20 @@ import { TabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
     });
 
     return (
