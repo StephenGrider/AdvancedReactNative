98f8d1284b5bf30828dea3273aa0428b575b1690
stephengrider <ste.grider@gmail.com>
2017-03-29 11:14:49 -0700

    added navigator

--- jobs/main.js
+++ jobs/main.js
@@ -1,12 +1,21 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
