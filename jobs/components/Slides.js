d809aa65b5a893e1969c3896e93313fc4b35a8e8
stephengrider <ste.grider@gmail.com>
2017-03-29 14:23:51 -0700

    Added styling to welcome screen

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 class Slides extends Component {
+  renderSlides() {
+    return this.props.data.map((slide) => {
+      return (
+        <View key={slide.text} style={styles.slideStyle}>
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-      </View>
+      <ScrollView
+        horizontal
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
     );
   }
 }
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  slideText: {
+    fontSize: 30
+  }
+};
 
 export default Slides;
