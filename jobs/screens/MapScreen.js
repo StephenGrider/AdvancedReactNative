7df3fc0d5fb3692954d3e287d9e907a9a52d7a46
stephengrider <ste.grider@gmail.com>
2017-03-30 13:35:39 -0700

    solve mapview issue

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
   state = {
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
@@ -12,7 +13,19 @@ class MapScreen extends Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <MapView
