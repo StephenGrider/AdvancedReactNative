0a198c28c578c7608a7374dba17e4f691346a741
stephengrider <ste.grider@gmail.com>
2017-03-30 14:46:45 -0700

    fetch list of jobs

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -6,6 +6,7 @@ import {
   FETCH_JOBS
 } from './types';
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
   publisher: '4201738803816157',
   format: 'json',
@@ -15,9 +16,18 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript'
 };
 
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = (region) => async (dispatch) => {
   try {
     let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    console.log(data);
   } catch(e) {
     console.error(e);
   }
