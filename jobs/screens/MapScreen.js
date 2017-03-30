ea7d23609c528af1f6538079f61a9ec265625f2b
stephengrider <ste.grider@gmail.com>
2017-03-30 13:26:49 -0700

    add default region prop

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -3,10 +3,22 @@ import { View, Text } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} />
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1 }}
+        />
       </View>
     );
   }
