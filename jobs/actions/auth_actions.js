dee76af6d64613e648c8604e905ebba06a07d1da
stephengrider <ste.grider@gmail.com>
2017-03-30 10:43:27 -0700

    crazy async await

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -8,6 +8,13 @@ import {
 // AsyncStorage.setItem('fb_token', token);
 // AsyncStorage.getItem('fb_token');
 
-export const facebookLogin = () => {
-  
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem('fb_token');
+
+  if (token) {
+    // Dispatch an action saying FB login is done
+  } else {
+    // Start up FB Login process
+
+  }
 };
