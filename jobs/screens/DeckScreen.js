38c4d43a611d2032eaf36943c3bc20591ee5b8c7
stephengrider <ste.grider@gmail.com>
2017-03-30 16:25:38 -0700

    props in deck screen

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 class DeckScreen extends Component {
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        
       </View>
     );
   }
 }
 
-export default DeckScreen;
+function mapStateToProps({ jobs }) {
+  return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
