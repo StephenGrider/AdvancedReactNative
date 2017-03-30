278c2ecde82db253ac806c1d7d7577e763527f0e
stephengrider <ste.grider@gmail.com>
2017-03-30 16:51:38 -0700

    added job card

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.jobtitle}>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
         <Swipe
           data={this.props.jobs}
+          renderCard={this.renderCard}
         />
       </View>
     );
   }
 }
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+};
 
 function mapStateToProps({ jobs }) {
   return { jobs: jobs.results };
