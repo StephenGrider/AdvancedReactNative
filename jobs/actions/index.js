13a3ed8ab68fb79a28dbdc61e348b15a388cb6a8
stephengrider <ste.grider@gmail.com>
2017-03-30 14:57:52 -0700

    added search button

--- jobs/actions/index.js
+++ jobs/actions/index.js
@@ -1,1 +1,2 @@
 export * from './auth_actions';
+export * from './job_actions';
--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -21,6 +25,10 @@ class MapScreen extends Component {
     this.setState({ region });
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  }
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -37,9 +45,27 @@ class MapScreen extends Component {
           style={{ flex: 1 }}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+export default connect(null, actions)(MapScreen);
