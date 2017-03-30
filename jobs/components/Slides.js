81867576952d525e39dd250181e35dc830a08b7b
stephengrider <ste.grider@gmail.com>
2017-03-30 09:21:14 -0700

    render button on last screen

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Onwards!"
+          raised
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map((slide) => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
           key={slide.text}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });
