77f9ff92449becd4d7e069c16c025774acc5c4e9
stephengrider <ste.grider@gmail.com>
2017-03-30 11:02:22 -0700

    finished fb login

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -1,7 +1,9 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
 } from './types';
 
 // How to use AsyncStorage:
@@ -13,8 +15,22 @@ export const facebookLogin = () => async dispatch => {
 
   if (token) {
     // Dispatch an action saying FB login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
     // Start up FB Login process
-
+    doFacebookLogin(dispatch);
   }
+};
+
+const doFacebookLogin = async dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync('196207057539134', {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 };
--- jobs/actions/types.js
+++ jobs/actions/types.js
@@ -1,1 +1,2 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
