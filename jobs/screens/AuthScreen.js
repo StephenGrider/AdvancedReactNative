838938b9eadcdc58b45bdec361450ae2f3e4dd66
stephengrider <ste.grider@gmail.com>
2017-03-30 11:58:22 -0700

    skip auth screen

--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -6,7 +6,17 @@ import * as actions from '../actions';
 class AuthScreen extends Component {
   componentDidMount() {
     this.props.facebookLogin();
-    AsyncStorage.removeItem('fb_token');
+    this.onAuthComplete(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.onAuthComplete(nextProps);
+  }
+
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate('map');
+    }
   }
 
   render() {
@@ -23,4 +33,8 @@ class AuthScreen extends Component {
   }
 }
 
-export default connect(null, actions)(AuthScreen);
+function mapStateToProps({ auth }) {
+  return { token: auth.token };
+}
+
+export default connect(mapStateToProps, actions)(AuthScreen);
