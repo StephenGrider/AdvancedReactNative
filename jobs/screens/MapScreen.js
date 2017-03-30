01e44ad841728b526722fc491e7089f916a2d5a6
stephengrider <ste.grider@gmail.com>
2017-03-30 13:47:24 -0700

    update region

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -17,6 +17,10 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true });
   }
 
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -31,6 +35,7 @@ class MapScreen extends Component {
         <MapView
           region={this.state.region}
           style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
         />
       </View>
     );
