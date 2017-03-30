497dd2d47f088fe44b7f34f920546f0501e3edd9
stephengrider <ste.grider@gmail.com>
2017-03-30 14:38:26 -0700

    convert latlong to zip

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
 
 import {
   FETCH_JOBS
 } from './types';
 
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
 export const fetchJobs = (region) => async (dispatch) => {
-  
+  try {
+    let zip = await reverseGeocode(region);
+  } catch(e) {
+    console.error(e);
+  }
 };
