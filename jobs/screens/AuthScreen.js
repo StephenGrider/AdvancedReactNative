78865f59ba7464ffb599b322f4c86e97bb4cca21
stephengrider <ste.grider@gmail.com>
2017-03-30 12:04:27 -0700

    finished auth screen

--- jobs/screens/AuthScreen.js
+++ jobs/screens/AuthScreen.js
@@ -21,14 +21,7 @@ class AuthScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-      </View>
+      <View />
     );
   }
 }
