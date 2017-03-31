f83746cc1e20050830c36b6136b04b8b53aeb6c5
stephengrider <ste.grider@gmail.com>
2017-03-30 17:11:12 -0700

    customize mapview in card

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView } from 'expo';
 import { Card, Button } from 'react-native-elements';
@@ -7,8 +7,24 @@ import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
   renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
     return (
       <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          >
+          </MapView>
+        </View>
         <View style={styles.detailWrapper}>
           <Text>{job.company}</Text>
           <Text>{job.formattedRelativeTime}</Text>
@@ -20,12 +36,20 @@ class DeckScreen extends Component {
     );
   }
 
+  renderNoMoreCards() {
+    return (
+      <Card title="No more jobs">
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
         <Swipe
           data={this.props.jobs}
           renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
         />
       </View>
     );
