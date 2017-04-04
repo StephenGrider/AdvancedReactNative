01435159153a264335c44155ce3dd211c9364f20
stephengrider <ste.grider@gmail.com>
2017-04-04 10:30:11 -0700

    navigate user back to map

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -39,7 +39,14 @@ class DeckScreen extends Component {
 
   renderNoMoreCards() {
     return (
-      <Card title="No more jobs">
+      <Card title="No More Jobs">
+        <Button
+          title="Back To Map"
+          large
+          icon={{ name: 'my-location' }}
+          backgroundColor="#03A9F4"
+          onPress={() => this.props.navigation.navigate('map')}
+        />
       </Card>
     );
   }
