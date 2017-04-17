f73c1aab3c5f434762602ff7e4c336489231a0ba
Stephen Grider <ste.grider@gmail.com>
2017-04-17 13:55:35 -0700

    Update twilio.js

--- auth-lambda/server/src/twilio.js
+++ auth-lambda/server/src/twilio.js
@@ -1,6 +1,7 @@
 const twilio = require('twilio');
 
 const accountSid = 'AC8f0fb862abeb3cdc9be5fbeadaf1a4d6';
+// This token has been revoked, don't even try it :)
 const authToken = 'bc369a180cb3e6e540df281a307fdc40';
 
 module.exports = new twilio.Twilio(accountSid, authToken); 
