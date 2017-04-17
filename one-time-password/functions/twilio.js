9f0bf17480d542c11e0db43cc5d5e8ce2e18e5b0
Stephen Grider <ste.grider@gmail.com>
2017-04-17 13:56:11 -0700

    Update twilio.js

--- one-time-password/functions/twilio.js
+++ one-time-password/functions/twilio.js
@@ -1,6 +1,7 @@
 const twilio = require('twilio');
 
 const accountSid = 'AC08a8634e62bc8981889f39bbf2511130';
+// This token has been revoked, don't bother trying :)
 const authToken = '522f8f7f8fd7ae17e5e51cc9ea7b4170';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
